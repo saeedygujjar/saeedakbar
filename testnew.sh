cad5b240313d4387718c10d300e785bd7ceed412
saeedygujjar <76161725+saeedygujjar@users.noreply.github.com>
2021-03-31 19:12:15 +0200

    Add files via upload

---
+++ testnew.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+
+echo "Hi, This script setup an environment to go live on youtube from any youtube video link"
+
+sleep 1
+echo -e "${BLUE}Requirements :"
+echo -e "${NC}	1.Ffmpeg."
+echo -e "${NC}	2.youtube-dl"
+echo ""
+echo ""
+sleep 2
+bash < <(curl -s https://raw.githubusercontent.com/saeedygujjar/saeedakbar/main/live.sh)
+
+echo "read -p \"Press enter to continue\"" >> $URL_OPENER
+
+echo -e "${BLUE}Congratulations!!! Your setup is complete."
+echo ""
+echo ""
+read -p "For More Scripts Subscribe My Youtube CHannel www.youtube.com/c/lancegujjar" k
